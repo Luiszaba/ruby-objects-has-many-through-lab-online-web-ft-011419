7bc5f27dcd0dffb816c30043a8fc2d65d3d32692
Luis Zabala <luis.zabala076@gmail.com>
2019-01-30 23:37:29 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -12,4 +12,17 @@ class Genre
   def self.all
     @all
   end
+  
+  def songs(song)
+    song.map do |songs|
+      if songs.genre == genre
+        songs
+      end
+    end
+  end
+  
+  def artists
+    self.all.select {|a_songs| a_songs.artist == artist}
+  end
+  
 end
