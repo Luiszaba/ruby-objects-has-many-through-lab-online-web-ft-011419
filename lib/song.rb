c7f55a86132d61086ab675d04ca8130430149b34
Luis Zabala <luis.zabala076@gmail.com>
2019-01-30 23:52:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 class Song
   
-  @@all = []
-  
   attr_accessor :name, :artist, :genre
   
-  def initialize(name)
+  @@all = []
+  
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
