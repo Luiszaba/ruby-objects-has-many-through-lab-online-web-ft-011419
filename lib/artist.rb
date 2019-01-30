745651a194e8aa3cffa0318875273878a4c69f12
Luis Zabala <luis.zabala076@gmail.com>
2019-01-30 23:48:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,5 +15,16 @@ def self.all
   @@all
 end
 
-
+def new_song(name, genre)
+  Song.new(name, self, genre)
 end
+
+def songs
+  Song.all.select {|playlist| playlist.artist == self}
+end
+
+def genres
+  songs.collect {|playlist-g| playlist-g.genre}
+end
+end
+
