8390e45664853bec87fe24e540173e80671b35d6
Luis Zabala <luis.zabala076@gmail.com>
2019-01-31 00:35:26 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -4,24 +4,11 @@ attr_accessor :name
 
 @@all = []
 
-def initialize(name)
-  @name = name
-  @@all << self
-end
-
-def new_appointment(patient, date)
-  Appointment.new(date, patient, self)
-end
-
-def appointments
-  Appointment.all.select {|appt| appt.doctor == self}
-end
-
-def patients
-  appointments.collect {|appt| appt.patient}
-end
 
 def self.all
   @all
 end
+
+
+
 end
