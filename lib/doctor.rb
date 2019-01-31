c58e0e1955e50369c7ac54fba3f3e71855f6bd5e
Luis Zabala <luis.zabala076@gmail.com>
2019-01-31 00:35:37 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -5,14 +5,13 @@ attr_accessor :name
 @@all = []
 
 
-def self.all
-  @all
-end
 
 def initialize(name)
   @name = name
   @@all << self
 end
+
+
 
 def new_appointment(patient, date)
   Appointment.new(date, patient, self)
