5a16216f9914a40719df20c5c47e34d1f26e784c
Luis Zabala <luis.zabala076@gmail.com>
2019-01-31 00:22:07 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,6 +13,14 @@ def new_appointment(patient, date)
   Appointment.new(date, patient, self)
 end
 
+def appointments
+  Appointment.all.select {|appt| appt.doctor == self}
+end
+
+def patients
+  appointments.collect {|appt| appt.patient}
+end
+
 def self.all
   @all
 end
